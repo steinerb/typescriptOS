c66127553fab55dc86573fac311df6a02224a471
Brian Steiner <briansteiner95@gmail.com>
2017-12-08 19:47:08 -0500

    removed ambiguous comments, ffsd still almost complete. file system needs to be implemented as well.

--- source/os/deviceDriverFileSystem.ts
+++ source/os/deviceDriverFileSystem.ts
@@ -13,7 +13,6 @@ module TSOS
 
             // The code below cannot run because "this" can only be
             // accessed after calling super.
-            //super(this.krnKbdDriverEntry, this.krnKbdDispatchKeyPress);
             super();
             this.driverEntry = this.krnFileSystemDriverEntry;
             this.isr = this.testISR;
--- source/os/deviceDriverKeyboard.ts
+++ source/os/deviceDriverKeyboard.ts
@@ -22,7 +22,6 @@ module TSOS
 
             // The code below cannot run because "this" can only be
             // accessed after calling super.
-            //super(this.krnKbdDriverEntry, this.krnKbdDispatchKeyPress);
             super();
             this.driverEntry = this.krnKbdDriverEntry;
             this.isr = this.krnKbdDispatchKeyPress;
